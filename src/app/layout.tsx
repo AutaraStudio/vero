94064586a2ec3ebdf99c05d2b273507636b0ead4
Autara Studio <matt@autara.studio>
2026-03-30 12:16:32 +0100

    u

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import './utilities.css';
 import SmoothScroll from '@/components/SmoothScroll';
 
 export const metadata: Metadata = {
